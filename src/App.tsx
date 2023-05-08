ac858ac55239d05ea1ceafaef9f44a84e19605a1
Amos Boldor <amosboldor@protonmail.com>
2023-05-07 21:24:09 -0700

    style photos library select button

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import { createSignal } from "solid-js";
 import { invoke } from "@tauri-apps/api/tauri";
 
 function App() {
-  const [photoPath, setPhotoPath] = createSignal("");
+  const [photoPath, setPhotoPath] = createSignal("No Library Selected");
 
   async function photosLibraryDialog() {
     setPhotoPath(await invoke("photos_library_dialog"));
   }
 
   return (
-    <div class="text-center">
-      <button type="button" onClick={() => photosLibraryDialog()}>
-        Select Photos Library
-      </button>
-      <p>{photoPath()}</p>
+    <div class="p-4">
+      <div class="border-4 border-slate-600 rounded-lg flex w-max">
+        <button class="flex-none mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-l-sm" type="button" onClick={() => photosLibraryDialog()}>
+          Select Photos Library
+        </button>
+        <div class="grow p-2">{photoPath()}</div>
+      </div>
     </div>
   );
 }
